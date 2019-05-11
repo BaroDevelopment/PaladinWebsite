b000d074f1c503d0f5dec8e9044dd90b703ba801
BARO <baris.simonjan@gmail.com>
2019-05-11 17:06:37 +0200

    Initial commit

---
+++ pages/documentation.vue
@@ -0,0 +1,19 @@
+<template>
+  <iframe src="https://www.paladinbot.online/docs" class="docs"></iframe>
+</template>
+
+<script>
+
+  export default {
+    name: "Documentation",
+    layout: 'documentation'
+  }
+</script>
+
+<style scoped>
+  .docs {
+    position: absolute;
+    width: 90%;
+    height: 95%;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-container fill-height>
+    <v-layout>
+      <v-flex xl10 offset-xl1>
+        <v-carousel>
+          <v-carousel-item
+            v-for="(item,i) in items"
+            :key="i"
+            :src="item.src"
+            reverse-transition="fade"
+            transition="fade"
+          >
+            <v-layout align-center justify-center column mt-5>
+              <div class="display-3">Welcome to Paladin</div>
+            </v-layout>
+            <v-layout align-space-between justify-center row fill-height wrap>
+              <v-btn large href="https://paladinbot.online/docs" target="_blank" class="ma-5">
+                <v-icon left>fas fa-book</v-icon>
+                Documentation
+              </v-btn>
+              <v-btn large href="https://paladinbot.online/forum" target="_blank" class="ma-5">
+                <v-icon left>fas fa-link</v-icon>
+                Forum
+              </v-btn>
+            </v-layout>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          {
+            src: 'https://cdn.discordapp.com/attachments/396964573007052800/556661928332623890/SLIDER.png'
+          },
+          {
+            src: 'https://cdn.discordapp.com/attachments/396964573007052800/576786486649946163/discord___wallpaper_by_mitcmack_da0ylfd-fullview.png'
+          },
+        ],
+      }
+    },
+    mounted () {
+      const next = document.querySelector(".v-carousel__next");
+      const prev = document.querySelector(".v-carousel__prev");
+
+      next.setAttribute("title", "Next");
+      prev.setAttribute("title", "Previous");
+    },
+  }
+</script>
+
+<style lang="stylus" scoped>
+  #example-custom-transition
+    .fade
+      &-enter-active, &-leave-active, &-leave-to
+        transition: .3s ease-out
+        position: absolute
+        top: 0
+        left: 0
+
+      &-enter, &-leave, &-leave-to
+        opacity: 0
+</style>
